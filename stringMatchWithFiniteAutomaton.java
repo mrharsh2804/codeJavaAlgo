052f8b2bf1ecd44ac334ebd13fa0e0dc46cd8b31
Harshal M Karande <51014492+mrharsh2804@users.noreply.github.com>
2020-11-27 22:22:58 -0800

    Update stringMatchWithFiniteAutomaton.java

--- stringMatchWithFiniteAutomaton.java
+++ stringMatchWithFiniteAutomaton.java
@@ -20,7 +20,39 @@ public class Main {
         
         return trans;
     }
+    /**
+    trying to find prefix == suffix. 
+    Ex.   a b a c 
+    state 0 1 2 3 4
     
+    lets say, we matched aba and we are now prcessing state 3.
+    let's say our next char in the string is b (and not c)
+    so here we find prefix == suffix to go to that state.
+    in this example, we need to return 1.
+                (ns start from 3. look for a b before this one. if its not be, ns-- (lines 41-43)) 
+          a b a b 
+    state 0 1 2 3 4
+            (found b. try to match the prefix [0:ns] with the suffix [state-ns+1+0:state-ns+1+ns]. If matched, return ns (the index of pattern string where we found b.).
+          a b a b 
+    state 0 1 2 3 4
+    
+    Prefix == suffix part explaination:
+    Prefix [0:ns] is easy to understand.
+    Suffix:
+    State (where we started from) - ns(where we got the matching char) + i (to move forward as prefix matches suffix)
+    Lets see this with other example
+            ns    state
+          a b c a b
+    state 0 1 2 3 4 5
+    We need to match ns letters from front. so go back ns from state. thats start of suffix. 0 is start of prefix. now move ahead 1 char at a time using i.
+    when i == ns, i.e. while searching when we reach the we know we have matched all chars in prefix to suffix. so return ns. 
+    
+    In the code we use ns-1 because the state as input is the state that is yet being processed. so the matched part so far is state-1. but in the for loop,
+    ns starts with state and not state -1. hence, we use ns-1 everywhere in that for loop.
+    (Note: While actually coding, just think of it as if you are given 1 based the index and not 0 baseed. so you need to do -1 on the indices. Its not the correct
+    reason why. but it makes coding easier.)
+    
+    */
     private static int nextState(char[] p, int m, int state, int a)
     {
         if(state < m && a == p[state]) return state+1;
@@ -31,7 +63,7 @@ public class Main {
             {
                 for(int i=0; i<ns-1; i++)
                 {
-                    if(p[i]!=p[state-ns+1+i]) break; //state (where we are) - ns+1 (number of chars in prefix) + i (to match each char in the suffix with prefix)
+                    if(p[i]!=p[state-ns+1+i]) break; //state (where we are) - ns (number of chars in prefix) + i (to match each char in the suffix with prefix)
                     if(i == ns-1) return ns;
                 }
             }
